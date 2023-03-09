f09f1abade2ab0c79d025ae5def20b60f6e5a410
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-03-09 17:52:05 +0600

    learnig react responsive website

---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBv7iHsIbwjI29G_wDcDhkdZ54kZHJWoow",
+    authDomain: "dragon-news-c3450.firebaseapp.com",
+    projectId: "dragon-news-c3450",
+    storageBucket: "dragon-news-c3450.appspot.com",
+    messagingSenderId: "248310761514",
+    appId: "1:248310761514:web:65491ff09d2b2b10601b53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
