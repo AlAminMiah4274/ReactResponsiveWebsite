49427479be0e4ad239b5eef9bbe2b22bf192350a
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-03-09 22:41:36 +0600

    modified some components and created many components

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h2>Footer</h2>
+            <p><small>Copyright coming soon</small></p>
         </div>
     );
 };
