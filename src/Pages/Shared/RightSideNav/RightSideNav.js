5e9f839493b04da8e3aae21afbede369e9e35aa7
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-03-11 10:53:10 +0600

    created some components and modified many component

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaWhatsapp, FaTwitch } from "react-icons/fa";
+import ListGroup from 'react-bootstrap/ListGroup';
+import BrandCarosel from '../BrandCarosel/BrandCarosel';
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2>Right Side Nav</h2>
+            <ButtonGroup vertical>
+                <Button className="mb-2" variant="outline-primary"><FaGoogle></FaGoogle> Log in Google</Button>
+                <Button variant="outline-dark"><FaGithub></FaGithub> Log in GitHub</Button>
+            </ButtonGroup>
+            <div>
+                <h5>Find us on</h5>
+
+                <ListGroup>
+                    <ListGroup.Item className='mb-2'><FaFacebook /> Facebook</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaWhatsapp /> WhatsApp</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitter /> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitch /> Twitch</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'>Vestibulum at eros</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <div>
+                <BrandCarosel></BrandCarosel>
+            </div>
         </div>
     );
 };
